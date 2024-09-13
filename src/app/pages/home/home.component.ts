af557604ba493a8a11ebbcf3db8b2cd68495ed65
dgouron <damien.gouron@gmail.com>
2024-09-13 11:58:45 +0200

    Feat : add stats for the homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, type OnInit } from "@angular/core";
 import { type Observable, of } from "rxjs";
+import { Olympic } from "src/app/core/models/Olympic";
 import { OlympicService } from "src/app/core/services/olympic.service";
 
 @Component({
@@ -9,10 +10,30 @@ import { OlympicService } from "src/app/core/services/olympic.service";
 })
 export class HomeComponent implements OnInit {
 	public olympics$: Observable<any> = of(null);
+	numberOfCountries = 0;
+	numberOfGames = 0;
 
 	constructor(private olympicService: OlympicService) {}
 
 	ngOnInit(): void {
 		this.olympics$ = this.olympicService.getOlympics();
+		this.olympics$.subscribe((olympics: Olympic[]) => {
+			this.calculateNumberOfGames(olympics);
+			this.calculateNumberOfCountries(olympics);
+		});
+	}
+
+	calculateNumberOfGames(olympics: Olympic[]): void {
+		const years = olympics.flatMap((olympic) =>
+			olympic.participations.map((participation) => participation.year),
+		);
+		const uniqueYears = new Set(years);
+		this.numberOfGames = uniqueYears.size;
+	}
+
+	calculateNumberOfCountries(olympics: Olympic[]): void {
+		this.numberOfCountries = new Set(
+			olympics.map((olympic: Olympic) => olympic.country),
+		).size;
 	}
 }
